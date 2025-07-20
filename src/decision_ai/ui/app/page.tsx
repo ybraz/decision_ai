77a2b6f70e370ca9e2847da9d1fbebe1f786089a
Yuri Rodrigues Braz <ybraz@live.com>
2025-07-20 15:59:55 -0300

    Rollback frontend

--- src/decision_ai/ui/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import CvDropzone from "@/components/CvDropzone";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <CvDropzone />
-    </main>
-  );
-}
